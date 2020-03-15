b9d918d8c55bba145ad976c3352d18e3d3a51884
recurshawn <shawnpereira1999@gmail.com>
2020-03-15 10:49:24 +0530

    Added spinning plus signs

--- src/App.js
+++ src/App.js
@@ -9,20 +9,35 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1 className="centerText">How dangerous is nCovid-19?</h1>
+        <h1 className="heading centerText">How dangerous is nCovid-19?</h1>
+        <div className="circle-plus-1">
+            <div className="circle rotate">
+              <div className="horizontal"></div>
+              <div className="vertical"></div>
+            </div>
+          </div>
         <div className="body">
-
           <p>Thousands more have died from Flu and traffic accidents so why is the Corona Virus (nCoVid-19) dangerous? Because we've only seen the tip of the iceberg.</p>
           <p>Insert stuff about exponential growth here</p>
-          <ExponentialGrowth/>
+          <ExponentialGrowth />
+          </div>
+          <div className="circle-plus-2">
+            <div className="circle rotate">
+              <div className="horizontal"></div>
+              <div className="vertical"></div>
+            </div>
+          </div>
+          <div className="body">
           <div className="block fixed">
             <p>I recommend watching this video by 3blue1brown (an amazing channel), especially if you want to gain a better intuition for the math behind epidemics.</p>
-            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/Kas0tIxDvrg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+            <div className="iframe-container">
+              <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/Kas0tIxDvrg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+            </div>
           </div>
-          <References/>
-          
+          <References />
+
         </div>
-        <Footer/>
+        <Footer />
       </div>
     );
   }
