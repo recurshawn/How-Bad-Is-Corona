073708332f84b8616f3ef9638fa7558200c14eba
recurshawn <shawnpereira1999@gmail.com>
2020-03-16 11:06:50 +0530

    YouTube embed fixed

--- src/components/References.js
+++ src/components/References.js
@@ -9,7 +9,7 @@ const References = () =>
         <p><a href="https://www.fast.ai/2020/03/09/coronavirus">Fast.ai - COVID-19: A data science perspective</a></p>
         <p>[If you know any good resources on the topic, tell me and I'll add them here]</p>
         <p>This site was heavily inspired by <a href="https://twitter.com/jajoosam">@jajoosam</a>'s letter to his school. Thanks Samarth!</p>
-        <p>Also, special thanks to my friend <a href="https://www.youtube.com/channel/UCDqPhm-wlsD902JVIjqo4GA">Kunal</a> for his inputs and <a href="https://twitter.com/thesephist">@thesephist</a>'s for blocks.css!</p>
+        <p>Also, speci;al thanks to my friend <a href="https://www.youtube.com/channel/UCDqPhm-wlsD902JVIjqo4GA">Kunal</a> for his inputs and <a href="https://twitter.com/thesephist">@thesephist</a>'s for blocks.css!</p>
     </div>) 
 
 }
--- src/components/YouTubeEmbed.js
+++ src/components/YouTubeEmbed.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import YouTube from 'react-youtube';
 
-const YouTubeEmbed = () => {
-    return (
-        <div className="block fixed">
-            <p>I recommend watching this video by 3blue1brown, especially if you want to gain a better intuition for the math behind epidemics.</p>
-            <div className="iframe-container">
-            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/Kas0tIxDvrg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+class YouTubeEmbed extends React.Component {
+    _onReady = (event) => {
+        // access to player in all event handlers via event.target
+        event.target.pauseVideo();
+    }
+    render() {
+        const opts = {
+            height: '390',
+            width: '640',
+            playerVars: { // https://developers.google.com/youtube/player_parameters
+                //autoplay: 1
+            }
+        };
+        return (
+            <div className="block fixed">
+                <p>I recommend watching this video by 3blue1brown, especially if you want to gain a better intuition for the math behind epidemics.</p>
+                <div className="iframe-container">
+                    <YouTube
+                    videoId="Kas0tIxDvrg"
+                    opts={opts}
+                    onReady={this._onReady}
+                    />
+                </div>
+                
             </div>
-        </div>
-    );
+        );
+    }
+    
 }
+
+
 
 export default YouTubeEmbed;
