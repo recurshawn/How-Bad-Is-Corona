4fbff7ccd3b6739101a3a94528c1118280642a16
recurshawn <shawnpereira1999@gmail.com>
2020-03-18 11:44:48 +0530

    Removed kofi link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="signature">
                 <p>Made with concern by Shawn Pereira <a href="https://twitter.com/Incept_shawn">@Incept_shawn</a>
                     <br />
-                <a href="https://shawnpereira.me">shawnpereira.me</a>
+                <a href="https://shawnpereira.me">shawnpereira.me</a></p>
         </div>
         </div>
     );
